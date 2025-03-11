76ff6bfaef6875dad1dabade1b5775016f011b64
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2025-03-12 02:00:15 +0500

    Creating Simple Project

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="flex justify-center items-center mt-40 text-9xl font-bold text-sky-600">
+        OsonBusiness
+      </div>
+      <div className="flex justify-center items-center mt-40 text-4xl font-bold text-sky-600">
+        Coming Soon...
+      </div>
+    </div>
+  );
+}
